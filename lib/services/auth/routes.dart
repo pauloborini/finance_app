59139694c8b10e7b24b101b775107fecc99e316d
pauloborini <86570695+pauloborini@users.noreply.github.com>
2022-11-19 09:10:05 -0300

    Delete routes.dart

--- lib/services/auth/routes.dart
+++
@@ -1,8 +0,0 @@
-class Routes {
-  Uri urlSignUp = Uri.parse(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBdq54K4lxgccnGN4okvIGmYNIe-_uZ1Xc");
-
-  Uri urlSignIn = Uri.parse(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBdq54K4lxgccnGN4okvIGmYNIe-_uZ1Xc");
-
-}
